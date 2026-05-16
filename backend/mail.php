c78c52ab0ace113ccdefa9c575e7406107dd1f72
ninya-rv <rhenahmaycabudlay@gmail.com>
2026-05-16 19:04:52 +0800

    Commit 1 file

--- backend/mail.php
+++ backend/mail.php
@@ -2,14 +2,12 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Resend;
-
 function sendActivationEmail($toEmail, $name)
 {
     try {
 
         $resend = Resend::client(
-            getenv('re_d9B5YisN_Er8TiJXnV52w1xSeBfnCSF41')
+            getenv('RESEND_API_KEY')
         );
 
         $safeName = htmlspecialchars($name);
@@ -37,35 +35,8 @@ function sendActivationEmail($toEmail, $name)
                     </p>
 
                     <p>
-                        Your student profile has been
-                        <b style='color:green'>ACTIVATED</b>
-                        by the administrator.
-                    </p>
-
-                    <p>
-                        You may now use your face for
-                        attendance verification.
-                    </p>
-
-                    <p>
-                        <b>Instruction:</b>
-                        Please proceed to your instructor
-                        and scan your face for attendance recording.
-                    </p>
-
-                    <div style='margin-top:15px;padding:10px;background:#f8f8f8;border-left:5px solid #800000'>
-
-                        <p style='margin:0'>
-                            <b>Status:</b> Active
-                        </p>
-
-                    </div>
-
-                    <br>
-
-                    <p style='font-size:12px;color:gray'>
-                        This is an automated message from
-                        EVSU Face Recognition Attendance System.
+                        Your account has been
+                        <b style='color:green'>ACTIVATED</b>.
                     </p>
 
                 </div>
